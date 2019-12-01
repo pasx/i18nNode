9b5bd142f9dff77cf0252b20857961a67892e923
pasx <pascal_guislain@yahoo.com>
2019-12-01 16:22:00 +0100

    Fixed reloading

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 let en = require('../i18n/i18n_en');
 let fr = require('../i18n/i18n_fr');
 
-export let i18n = {
-  loadI18n: function (language) {
-    console.log(language);
-    i18n.selectedLanguage = language;
-    switch (language) {
+let _ = {
+  load: (i18n, lang) => {
+    console.log(lang);
+    let data;
+    switch (lang) {
       case 'en':
-        i18n = en;
+        data = en;
         break;
       case 'fr':
-        i18n = fr;
+        data = fr;
         break;
       default:
-        throw (new Error('unsuported language: ' + language));
+        throw (new Error('unsuported language: ' + lang));
     }
-    console.log(JSON.stringify(i18n).substring(0, 300) + '...');
+    console.log(JSON.stringify(data).substring(0, 300) + '...');
+    return { loadI18n: i18n.loadI18n, selectedLanguage : lang ,...data };
   }
 };
 
+export let i18n = {
+  loadI18n: function (language) {
+    console.log(language);
+    i18n = _.load(i18n,language);
+  }
+};
 i18n.loadI18n('en');
 
 
